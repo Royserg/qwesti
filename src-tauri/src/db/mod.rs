8690967848d8613c504235d9d820a1a4e80b550f
jakub <staniojakub@gmail.com>
2025-01-29 20:05:28 +0100

    c: setup database in the run function

--- src-tauri/src/db/mod.rs
+++ src-tauri/src/db/mod.rs
@@ -1,30 +1,43 @@
 use std::path::Path;
 
-use sqlx::{migrate::MigrateDatabase, sqlite::SqlitePool, Sqlite};
-use tauri::{AppHandle, Manager};
+use sqlx::{
+    migrate::MigrateDatabase,
+    sqlite::{SqlitePool, SqlitePoolOptions},
+    Sqlite,
+};
+use tauri::{
+    path::{self, BaseDirectory, PathResolver},
+    AppHandle, Manager,
+};
 
-pub async fn init_db(app: &AppHandle) -> anyhow::Result<()> {
-    let resource_dir = app
-        .path()
-        .resource_dir()
-        .expect("Failed to get resource dir");
+pub async fn init_db() -> anyhow::Result<SqlitePool> {
+    // TODO: enable for deployment/built
+    let home_dir = dirs::home_dir().unwrap();
+    let db_path = home_dir.to_str().unwrap().to_string() + "/.config/qwesti/database.db";
+    // Development: current di
 
-    let db_path = resource_dir.to_owned().join("qwesti.db");
-    let db_path_str = db_path
-        .to_str()
-        .ok_or(anyhow::anyhow!("Failed to turn path into string"))?;
+    dbg!("+++++++++++++");
+    dbg!(db_path);
+    dbg!("+++++++++++++");
 
-    if !Path::new(db_path_str).exists() {
-        dbg!("DB file didn't exist, creating at: {:?}", db_path_str);
+    let dev_db_path = "database.db";
+    if !Path::new(dev_db_path).exists() {
+        dbg!("DB file didn't exist, creating at: {:?}", dev_db_path);
 
-        Sqlite::create_database(&db_path_str).await?;
+        Sqlite::create_database(&dev_db_path).await?;
     }
 
-    dbg!("Initializing database: {:?}", &db_path_str);
+    dbg!("Initializing database: {:?}", &dev_db_path);
 
-    let db_pool = SqlitePool::connect(&db_path_str).await?;
-    sqlx::migrate!().run(&db_pool).await?;
+    let pool = SqlitePoolOptions::new()
+        .max_connections(5)
+        .connect(&dev_db_path)
+        .await?;
 
-    Ok(())
-    // Ok(db_pool)
+    // // let db_pool = SqlitePool::connect(&db_path_str).await?;
+
+    sqlx::migrate!().run(&pool).await?;
+
+    Ok(pool)
+    // Ok(())
 }
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,41 +1,27 @@
 mod db;
 use specta_typescript::Typescript;
-use sqlx::sqlite::SqlitePoolOptions;
+use sqlx::{Pool, Sqlite};
 
 use db::init_db;
-use tauri::{path::PathResolver, AppHandle};
 use tauri_specta::{collect_commands, Builder};
+
+struct DbConnection {
+    db: Pool<Sqlite>,
+}
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub async fn run() -> anyhow::Result<()> {
-    // TODO: read from env
-    // let db_url = "sqlite://qwesti.db?mode=rwc".to_string();
-
-    // let pool = SqlitePoolOptions::new()
-    //     .max_connections(5)
-    //     .connect("postgres://postgres:password@localhost/test")
-    //     .await?;
-
-    // let conn = Database::connect(db_url)
-    //     .await
-    //     .expect("Database connection failed");
-
-    // Migrator::up(&conn, None).await.unwrap();
-
-    // let builder = Builder::<tauri::Wry>::new()
-    //     // Then register them (separated by a comma)
-    //     .commands(collect_commands![
-    //         create_connection,
-    //         get_connections,
-    //         get_today_bday_connections
-    //     ]);
-    // ---
-
-    // tauri::Builder::default()
-    //     .plugin(tauri_plugin_opener::init())
-    //     .invoke_handler(tauri::generate_handler![greet])
-    //     .run(tauri::generate_context!())
-    //     .expect("error while running tauri application");
+    let init_db_res = init_db().await;
+    let db_pool = match init_db_res {
+        Ok(pool) => {
+            println!("DB init");
+            pool
+        }
+        Err(_err) => {
+            println!("Failed to init DB");
+            panic!("Failed to init db")
+        }
+    };
 
     let builder = Builder::<tauri::Wry>::new()
         // Then register them (separated by a comma)
@@ -51,13 +37,10 @@ pub async fn run() -> anyhow::Result<()> {
         .expect("Failed to export typescript bindings");
 
     tauri::Builder::default()
-        // .manage(DbConnection { db: conn })
+        .manage(DbConnection { db: db_pool })
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(builder.invoke_handler())
-        // .setup(setup)
         .setup(move |app| {
-            let _ = setup(app);
-
             // This is required if you want to use events
             builder.mount_events(app);
             Ok(())
@@ -65,25 +48,6 @@ pub async fn run() -> anyhow::Result<()> {
         // on an actual app, remove the string argument
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
-
-    Ok(())
-}
-
-fn setup<'a>(app: &'a mut tauri::App) -> Result<(), Box<dyn std::error::Error>> {
-    let handle = app.handle().to_owned();
-
-    tauri::async_runtime::spawn(async move {
-        let init_db_res = init_db(&handle).await;
-        match init_db_res {
-            Ok(_) => {
-                println!("DB init")
-            }
-            Err(err) => {
-                println!("Failed to init DB");
-                println!("{}", err);
-            }
-        }
-    });
 
     Ok(())
 }
