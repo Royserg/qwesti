25e67c1429df08ecaf7f00f813de146ad3d7cd4a
jakub <staniojakub@gmail.com>
2025-02-09 13:49:59 +0100

    c: remove leftovers

--- src/service/quests/toggle-quest-completed.ts
+++
@@ -1,9 +0,0 @@
-import { Quest } from "~/models/quest";
-
-export const toggleQuestCompletedUseCase = async (input: {
-  id: number;
-}): Promise<Quest> => {
-  const updatedQuest = await questsRepository.toggleQuestCompleted(input.id);
-
-  return updatedQuest;
-};
