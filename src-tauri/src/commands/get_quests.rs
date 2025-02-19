ed6bc5b0dbb2700b7ff410e8a6c2b4fc339697e6
jakub <staniojakub@gmail.com>
2025-02-19 22:50:16 +0100

    c: split get_quests query into 2: today and custom date

--- src-tauri/src/commands/get_quests.rs
+++ src-tauri/src/commands/get_quests.rs
@@ -1,3 +1,4 @@
+use chrono::Local;
 use tauri::{command, State};
 
 use crate::entities::Quest;
@@ -12,15 +13,14 @@ pub async fn get_quests(
 ) -> Result<Vec<Quest>, String> {
     let date = date.unwrap_or_else(|| "now".to_string());
 
-    println!("REQUESTED DATE: {}", date);
+    let today_date = Local::now().format("%Y-%m-%d").to_string();
 
-    // WHERE
-    //     completed_at IS NULL
-    //     OR
-    //     Date(completed_at) = DATE($1)
-    let quests = sqlx::query_as!(
-        QuestRow,
-        r#"
+    // For dates in the past, show only completed Quests
+    // Not completed carry over to the current date
+    if date == today_date {
+        let quests = sqlx::query_as!(
+            QuestRow,
+            r#"
         SELECT
             id,
             title,
@@ -33,19 +33,45 @@ pub async fn get_quests(
         WHERE
             Date(completed_at) = DATE($1)
             OR
-            Date(created_at) = DATE($1) AND completed_at IS NOT NULL
-            OR
             (completed_at IS NULL AND Date(created_at) <= Date($1))
         ORDER BY
             created_at DESC
         "#,
-        date,
-    )
-    .fetch_all(&state.db)
-    .await
-    .expect("Failed to fetch quests");
+            date,
+        )
+        .fetch_all(&state.db)
+        .await
+        .expect("Failed to fetch quests");
 
-    let quests = quests.into_iter().map(Quest::from).collect();
+        let quests = quests.into_iter().map(Quest::from).collect();
 
-    Ok(quests)
+        Ok(quests)
+    } else {
+        let quests = sqlx::query_as!(
+            QuestRow,
+            r#"
+        SELECT
+            id,
+            title,
+            completed,
+            created_at,
+            completed_at,
+            order_index
+        FROM
+            quests
+        WHERE
+            Date(completed_at) = DATE($1)
+        ORDER BY
+            created_at DESC
+        "#,
+            date,
+        )
+        .fetch_all(&state.db)
+        .await
+        .expect("Failed to fetch quests");
+
+        let quests = quests.into_iter().map(Quest::from).collect();
+
+        Ok(quests)
+    }
 }
