0e91ff1d31ea7905658bdc3c8053452b46286405
jakub <staniojakub@gmail.com>
2025-02-25 19:03:29 +0100

    c: add BaseLayout

---
+++ src/layouts/base.tsx
@@ -0,0 +1,15 @@
+import { Component, ParentProps } from "solid-js"
+import { cn } from "~/lib/utils";
+
+interface Props extends ParentProps {
+  class?: string;
+}
+
+export const BaseLayout: Component<Props> = (props) => {
+  return (
+    <main class={cn(props.class)}>
+      {props.children}
+    </main>
+  )
+}
+
