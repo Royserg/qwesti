287de88ce8bd026f9a1e31fd9743695465f0c6c5
jakub <staniojakub@gmail.com>
2025-03-04 17:42:12 +0300

    s: set container to take up all the window space

--- src/layouts/base.tsx
+++ src/layouts/base.tsx
@@ -7,7 +7,7 @@ interface Props extends ParentProps {
 
 export const BaseLayout: Component<Props> = (props) => {
   return (
-    <main class={cn(props.class)}>
+    <main class={cn('h-full w-full', props.class)}>
       {props.children}
     </main>
   )
