3358aade24a60263641b0800cfa8eb72d8ec02a0
jakub <staniojakub@gmail.com>
2025-03-22 09:40:19 +0100

    c: move filters to a new file

--- src/components/quests-list.tsx
+++ src/components/quests-list.tsx
@@ -2,15 +2,13 @@ import {
   Component,
   ErrorBoundary,
   For,
-  ParentComponent,
   Show
 } from "solid-js";
 import { Quest } from "~/bindings";
-import { cn } from "~/lib/utils";
+import { QuestsFilterEnumType } from "~/routes";
 import { isTodaySelected } from "~/stores/date";
+import { Filters } from "./filters";
 import { QuestCard } from "./quest-card";
-import { useNavigate } from "@tanstack/solid-router";
-import { QuestsFilterEnum, QuestsFilterEnumType } from "~/routes";
 
 
 interface Props {
@@ -59,66 +57,3 @@ export const QuestsList: Component<Props> = (props) => {
   );
 };
 
-// -- Filter Button --
-interface FiltersProps {
-  filter: string;
-}
-const Filters: Component<FiltersProps> = (props) => {
-  const filters = [
-    {
-      value: QuestsFilterEnum.enum.all,
-      label: "all",
-    },
-    {
-      value: QuestsFilterEnum.enum.pending,
-      label: "pending",
-    },
-    {
-      value: QuestsFilterEnum.enum.completed,
-      label: "completed",
-    },
-  ];
-
-  return (
-    <div class="mx-auto flex w-full justify-center gap-3">
-      <For each={filters}>
-        {(filter) => (
-          <FilterButton
-            value={filter.value}
-            active={filter.value === props.filter}
-          >
-            {filter.label}
-          </FilterButton>
-        )}
-      </For>
-    </div>
-  );
-};
-
-interface FilterButtonProps {
-  active?: boolean;
-  value: QuestsFilterEnumType;
-}
-const FilterButton: ParentComponent<FilterButtonProps> = (props) => {
-  const navigate = useNavigate({ from: '/' });
-
-  const handleClick = () => {
-    navigate({ to: '/', search: { filter: props.value }, replace: true });
-  };
-
-  return (
-    <button
-      onClick={handleClick}
-      class={cn(
-        "bg-background flex-1 cursor-pointer rounded-xs border px-4 py-1 transition-colors",
-        {
-          "bg-amber-200 font-medium shadow-md": props.active,
-          "focus:bg-background focus:text-foreground hover:bg-background hover:text-foreground bg-gray-200/30 text-gray-400":
-            !props.active,
-        },
-      )}
-    >
-      {props.children}
-    </button>
-  );
-};
