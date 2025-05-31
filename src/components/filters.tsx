250b7192f93d02eeecae755b3bbd88d5fa8daa1f
jakub <staniojakub@gmail.com>
2025-05-31 12:18:24 +0200

    show selected filter beside dropdown

--- src/components/filters.tsx
+++ src/components/filters.tsx
@@ -1,6 +1,11 @@
 import { useNavigate } from "@tanstack/solid-router";
-import ChevronDown from 'icons/chevron-down';
-import { type Component, createSignal, For, type ParentComponent } from "solid-js";
+import ChevronDown from "icons/chevron-down";
+import {
+  type Component,
+  createSignal,
+  For,
+  type ParentComponent,
+} from "solid-js";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -8,7 +13,7 @@ import {
   DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuSeparator,
-  DropdownMenuTrigger
+  DropdownMenuTrigger,
 } from "~/components/ui/dropdown-menu";
 import { cn } from "~/lib/utils";
 import { QuestsFilterEnum, type QuestsFilterEnumType } from "~/routes";
@@ -36,39 +41,43 @@ export const Filters: Component<FiltersProps> = (props) => {
   ];
 
   return (
-    <div class="mx-auto relative w-full flex justify-end">
+    <div class="relative mx-auto flex w-full justify-end">
+      <div class="flex items-center gap-2">
+        <span class="rounded-xs bg-amber-200 px-2 py-1 text-sm font-medium shadow-md">
+          {props.filter}
+        </span>
 
-      <DropdownMenu onOpenChange={setIsOpen}>
-        <DropdownMenuTrigger class="flex items-center gap-1 bg-amber-200 font-medium rounded-xs text-sm border px-4 py-2 shadow-md transition-colors hover:bg-amber-300">
-          <ChevronDown
-            class={cn("size-4 transition-transform", {
-              "rotate-180": isOpen(),
-            })}
-          />
-          Filters
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuLabel>Status</DropdownMenuLabel>
-          <DropdownMenuSeparator />
+        <DropdownMenu onOpenChange={setIsOpen}>
+          <DropdownMenuTrigger class="flex items-center gap-1 rounded-xs border bg-amber-200 px-4 py-2 text-sm font-medium shadow-md transition-colors hover:bg-amber-300">
+            <ChevronDown
+              class={cn("size-4 transition-transform", {
+                "rotate-180": isOpen(),
+              })}
+            />
+            Filters
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>Status</DropdownMenuLabel>
+            <DropdownMenuSeparator />
 
-          <DropdownMenuGroup class="flex flex-col p-1 gap-1">
-            <For each={filters}>
-              {(filter) => (
-                <DropdownMenuItem class="p-0">
-                  <FilterButton
-                    value={filter.value}
-                    active={filter.value === props.filter}
-                    onClick={() => setIsOpen(false)}
-                  >
-                    {filter.label}
-                  </FilterButton>
-                </DropdownMenuItem>
-              )}
-            </For>
-          </DropdownMenuGroup>
-
-        </DropdownMenuContent>
-      </DropdownMenu>
+            <DropdownMenuGroup class="flex flex-col gap-1 p-1">
+              <For each={filters}>
+                {(filter) => (
+                  <DropdownMenuItem class="p-0">
+                    <FilterButton
+                      value={filter.value}
+                      active={filter.value === props.filter}
+                      onClick={() => setIsOpen(false)}
+                    >
+                      {filter.label}
+                    </FilterButton>
+                  </DropdownMenuItem>
+                )}
+              </For>
+            </DropdownMenuGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
     </div>
   );
 };
@@ -82,7 +91,11 @@ const FilterButton: ParentComponent<FilterButtonProps> = (props) => {
   const navigate = useNavigate({ from: "/" });
 
   const handleClick = () => {
-    navigate({ to: "/", search: (prev) => ({ ...prev, filter: props.value }), replace: true });
+    navigate({
+      to: "/",
+      search: (prev) => ({ ...prev, filter: props.value }),
+      replace: true,
+    });
     if (props.onClick) props.onClick();
   };
 
@@ -91,11 +104,10 @@ const FilterButton: ParentComponent<FilterButtonProps> = (props) => {
       type="button"
       onClick={handleClick}
       class={cn(
-        "w-full cursor-pointer rounded-xs border px-4 py-2 transition-colors text-left",
+        "w-full cursor-pointer rounded-xs border px-4 py-2 text-left transition-colors",
         {
           "bg-amber-200 font-medium shadow-md": props.active,
-          "hover:bg-gray-100 bg-white text-gray-700":
-            !props.active,
+          "bg-white text-gray-700 hover:bg-gray-100": !props.active,
         },
       )}
     >
